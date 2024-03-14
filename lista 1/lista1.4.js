f8854f0bc505f04e3dcd8a9180c0fe747593cf30
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-14 00:00:47 +0000

    lista 1 exercicio 4

---
+++ lista 1/lista1.4.js
@@ -0,0 +1,5 @@
+var readline = require("readline-sync");
+var base = readline.questionInt("Digite a base: ");
+var altura = readline.questionInt("Digite a altura: ");
+var area = (base * altura);
+console.log(area)
