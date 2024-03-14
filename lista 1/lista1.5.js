15efdca56d3cd038ccf0aec0d452d1e2bd2112d9
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-14 13:37:35 +0000

    lista 1 exercicio 5

---
+++ lista 1/lista1.5.js
@@ -0,0 +1,8 @@
+//5. Calculadora de IMC
+//Peça ao usuário para inserir seu peso (em kg) e altura (em metros). Calcule o Índice de Massa Corporal (IMC) utilizando a fórmula IMC = peso / altura^2 e exiba o resultado.
+
+var readline = require("readline-sync");
+var peso = readline.questionFloat("Digite seu peso: ");
+var altura = readline.questionFloat("Digite sua altura: ");
+var imc = ((altura*altura)/peso)
+console.log(imc)
