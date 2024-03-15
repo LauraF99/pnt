62166bf407a617073465deddc21afd58ac65c45e
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-15 18:03:10 +0000

    lista 2 exercicio 2

---
+++ lista 2/lista2.2.js
@@ -0,0 +1,10 @@
+//Determinar se um aluno foi aprovado ou reprovado (média >= 6,00, duas notas).
+
+var nota1 = 10
+var nota2 = 7
+var media = (nota1+nota2)/2
+if (media >=6) {
+    console.log("Aluno aprovado")
+} else {
+    console.log("Aluno reprovado")
+}
