46daf51518a729563b6679787e3ac8e20780aea9
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-15 17:34:20 +0000

    lista 2 exercicio 1

---
+++ lista 2/lista2.1.js
@@ -0,0 +1,10 @@
+//Verificar se um número é positivo, negativo ou zero.
+
+var numero = -5
+if (numero > 0) {
+    console.log(" o número é positivo")
+} else if (numero = 0) {
+    console.log("o número é zero")
+} else  {
+    console.log("o número é negativo")
+}
