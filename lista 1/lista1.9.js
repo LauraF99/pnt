6d5978889a80aea001a951fac137c610b6dc4d92
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-15 16:36:09 +0000

    lista 1 exercicio 9

---
+++ lista 1/lista1.9.js
@@ -0,0 +1,8 @@
+//9. Tempo de viagem
+//Peça ao usuário a distância em quilômetros e a velocidade média em km/h. Calcule o tempo estimado de viagem e mostre-o ao usuário.
+
+var readline = require("readline-sync");
+var distância = readline.questionInt("Digite a distância em km: ");
+var velocidade = readline.questionInt("Digite a velocidade média em km/h: ");
+var tempo = distância/velocidade
+console.log("o tempo estimado é de" + " " + tempo + " horas")
