344fca579de70f5fedad31d2129803880f92b1bc
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-15 16:26:36 +0000

    lista 1 exercicio 8

---
+++ lista 1/lista1.8.js
@@ -0,0 +1,22 @@
+//8. Tabuada
+//Peça ao usuário para inserir um número inteiro e exiba a tabuada desse número, de 1 a 10.
+
+var readline = require("readline-sync");
+var numero = readline.questionInt("Digite um número: ");
+console.log(numero + "x" + 1 + "=" + numero*1);
+console.log(numero + "x" + 2 + "=" + numero*2);
+console.log(numero + "x" + 3 + "=" + numero*3);
+console.log(numero + "x" + 4 + "=" + numero*4);
+console.log(numero + "x" + 5 + "=" + numero*5);
+console.log(numero + "x" + 6 + "=" + numero*6);
+console.log(numero + "x" + 7 + "=" + numero*7);
+console.log(numero + "x" + 8 + "=" + numero*8);
+console.log(numero + "x" + 9 + "=" + numero*9);
+console.log(numero + "x" + 10 + "=" + numero*10);
+
+
+
+
+
+
+
