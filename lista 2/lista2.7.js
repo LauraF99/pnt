fda08dfc5b928dfe8bb58bd55e3a9bd16a316c1c
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-20 17:34:26 +0000

    lista 2 exercicio 7

--- lista 2/lista2.7.js
+++ lista 2/lista2.7.js
@@ -1,2 +1,14 @@
 //Atribuir uma categoria de acordo com a idade de uma pessoa (criança, adolecente, adulto e idoso).
 
+var readline = require("readline-sync");
+var idade = readline.questionInt("Digite sua idade: ");
+
+if (idade < 13) {
+    console.log("Criança");
+} else if (idade < 18) {
+    console.log("Adolescente");
+} else if (idade < 64) {
+    console.log("Adulto");
+} else {
+    console.log("Idoso");
+}
