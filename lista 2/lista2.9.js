da16428f38b72ec8b0c8a3a17086d85d849a5a0e
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-20 23:34:00 +0000

    exerc

--- lista 2/lista2.9.js
+++ lista 2/lista2.9.js
@@ -1,1 +1,16 @@
 //Verificar se um número é primo ou não (divisível por 1 e por ele mesmo).
+var readline = require("readline-sync");
+
+var numero = readline.questionInt("Digite o numero: ");
+
+var primo1 = numero % 2;
+var primo2 = numero % 3;
+var primo3 = numero % 5;
+
+if ((primo1 == 0 && primo1 > 2) || (primo2 == 0 && primo2 > 3) || (primo3 == 0 && primo3 > 5) ) {
+    console.log("Este número não é primo");
+} else if (numero == 1 || numero == 0) {
+    console.log("Este numero não é primo");
+} else {
+    console.log("Este número é primo");
+}
