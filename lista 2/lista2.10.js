96366bfa552c60ce0b62f39da420d45b1f233195
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-20 23:33:28 +0000

    exerc 10

--- lista 2/lista2.10.js
+++ lista 2/lista2.10.js
@@ -1,1 +1,10 @@
 //Determinar a estação do ano com base no mês (primavera, verão, outono e inverno).
+
+var readline = require("readline-sync");
+var mesDoAno = readline.question("Digite o mês do ano: ");
+
+if (mesDoAno == janeiro && mesDoAno == fevereiro OU) {
+    
+} else {
+    
+}
