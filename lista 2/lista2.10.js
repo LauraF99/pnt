bb9f49b2b8f0d20644baf9c70b0e164f3dc2c332
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-21 13:49:37 +0000

    lista 2 exercicio 9

--- lista 2/lista2.10.js
+++ lista 2/lista2.10.js
@@ -1,10 +1,16 @@
 //Determinar a estação do ano com base no mês (primavera, verão, outono e inverno).
 
 var readline = require("readline-sync");
-var mesDoAno = readline.question("Digite o mês do ano: ");
+var mesDoAno = readline.question(" Digite o mês do ano em número: ");
 
-if (mesDoAno == janeiro && mesDoAno == fevereiro OU) {
+
+if (mesDoAno == 1 || mesDoAno == 2 || mesDoAno == 3 ) {
     
 } else {
     
 }
+
+var num1 = 0
+var num2 = "zero"
+num1 == num2
+num1 === num2 
--- lista 2/lista2.9.js
+++ lista 2/lista2.9.js
@@ -7,7 +7,7 @@ var primo1 = numero % 2;
 var primo2 = numero % 3;
 var primo3 = numero % 5;
 
-if ((primo1 == 0 && primo1 > 2) || (primo2 == 0 && primo2 > 3) || (primo3 == 0 && primo3 > 5) ) {
+if (primo1 == 0 || primo1 > 2 && primo2 == 0 || primo2 > 3 && primo3 == 0 || primo3 > 5 ) {
     console.log("Este número não é primo");
 } else if (numero == 1 || numero == 0) {
     console.log("Este numero não é primo");
