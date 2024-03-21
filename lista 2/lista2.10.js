24fbca79dd4878959ec75c4badbbcd195678984d
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-21 13:53:31 +0000

    lista 2 exercicio 10

--- lista 2/lista2.10.js
+++ lista 2/lista2.10.js
@@ -5,12 +5,12 @@ var mesDoAno = readline.question(" Digite o mês do ano em número: ");
 
 
 if (mesDoAno == 1 || mesDoAno == 2 || mesDoAno == 3 ) {
-    
+    console.log("Estamos no verão");
+} else if (mesDoAno == 4 || mesDoAno == 5 || mesDoAno == 6) {
+    console.log("Estamos no outono");
+} else if (mesDoAno == 7 || mesDoAno == 8 || mesDoAno == 9) {
+    console.log("Estamos no inverno")
 } else {
-    
+    console.log("Estamos na primavera")
 }
 
-var num1 = 0
-var num2 = "zero"
-num1 == num2
-num1 === num2 
