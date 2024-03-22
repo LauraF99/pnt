f3895c75f66c381590dc1fb5c4374d8d9806ba54
R4incife <160287024+R4incife@users.noreply.github.com>
2024-03-22 15:15:44 +0000

    lista 4 exercicio 2

---
+++ lista 4/lista4.2.js
@@ -0,0 +1,24 @@
+/*2. Questão de adivinhação de número: 
+Desenvolva um jogo em que o computador escolhe um número aleatório entre 1 e 100, 
+e o jogador deve adivinhar qual é esse número. 
+O jogador recebe dicas se seu palpite é muito alto ou muito baixo, 
+e o jogo continua até que o número seja adivinhado*/
+
+var readline = require("readline-sync")
+var numAleatorio = Math.floor(Math.random() * 100) + 1;
+var palpites;
+
+
+while (numAleatorio !== palpites) {
+    palpites = readline.questionInt("Qual o seu palpite? (1-100): ");
+if (palpites > numAleatorio) {
+    console.log("Seu palpite é muito alto, tente novamente ")
+} else {
+    console.log("Seu palpite é muito baixo, tente novamente ")
+} 
+    
+}
+
+if (numAleatorio == palpites){
+    console.log("Parabéns, você acertou");
+}
