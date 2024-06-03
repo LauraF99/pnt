29013a4c9ab8626cb92329d2864516c3f7ee69ef
Laura Ferreira <160287024+LauraF99@users.noreply.github.com>
2024-06-03 22:53:58 +0000

    lista 7

--- lista5.js
+++ lista5.js
@@ -8,14 +8,16 @@ do {
 
 // 2. Questão de validação de entrada de usuário
 
-let readline = require("readline-sync");
+/*let readline = require("readline-sync");
 let login = "senha123";
+let senha;
 
 do {
-    let senha = readline.question("Digite a senha: ")
+    senha = readline.question("Digite a senha: ")
 } while (login !== senha);
+console.log("Senha correta");*/
+
+// 3. Questão de soma de números positivos: 
 
 
-/*while (senha !== login){
-    let senha = readline.question("Digite a senha: ")
-}*/
+
---
+++ lista7.js
@@ -0,0 +1,6 @@
+// Exercício 1: Função de Soma
+
+function soma (a, b) {
+    return a + b;
+}
+console.log(soma(2,5));
